e883119c9e7a64640f9c1e61702889aa8c929ba3
SagarPrasad160 <sagarprasad916@gmail.com>
2023-01-07 17:38:12 +0530

    complete classes task

--- index.js
+++ index.js
@@ -35,3 +35,30 @@ book.apply(swiss, bookingDetails);
 const bookSW = book.bind(swiss);
 
 bookSW(45, "Jhonny Depp");
+
+class Student {
+  constructor(name, age, phone, boardMarks) {
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.boardMarks = boardMarks;
+
+    this.index = Student.prototype.index++;
+  }
+
+  isEligible() {
+    if (this.boardMarks > 40) {
+      console.log("is Eligble");
+    } else {
+      console.log("Not eligible");
+    }
+  }
+}
+
+Student.prototype.index = 0;
+
+let Student1 = new Student("Yash", 24, 434214, 6);
+let Student2 = new Student("Shubham", 19, 9024481, 45);
+Student1.isEligible();
+
+//console.log(Student.prototype.index);
