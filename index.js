5878fa375375c56c92704f5cd01d65db7d761aad
SagarPrasad160 <sagarprasad916@gmail.com>
2023-01-08 14:00:23 +0530

    added style sheet

--- index.js
+++ index.js
@@ -62,3 +62,26 @@ let Student2 = new Student("Shubham", 19, 9024481, 45);
 Student1.isEligible();
 
 //console.log(Student.prototype.index);
+
+setTimeout(() => console.log("timer1 expired"), 1000);
+
+setTimeout(() => console.log("timer2 expired"), 0);
+
+function x(y) {
+  console.log("inside x");
+
+  y();
+}
+
+x(function y() {
+  setTimeout(() => console.log("inside y"), 0);
+});
+
+document.querySelector("button").addEventListener("click", function () {
+  console.log("clicked me");
+});
+
+//Rest and Spread Operators
+
+const [...arr] = [1, 2, 3, 4, 5];
+console.log(arr);
